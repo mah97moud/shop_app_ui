6d47a68ea9ab43603ca8f1612ce0e6d72e3f078c
mahmoud <mahmoudatif54@yahoo.com>
2021-08-27 21:22:34 +0200

    add hero

--- lib/modules/details/components/product_title_with_image.dart
+++ lib/modules/details/components/product_title_with_image.dart
@@ -55,9 +55,12 @@ class ProductTitleWithImage extends StatelessWidget {
                 ),
               ),
               Expanded(
-                child: Image.asset(
-                  product.image,
-                  fit: BoxFit.fill,
+                child: Hero(
+                  tag: '${product.id}',
+                  child: Image.asset(
+                    product.image,
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ],
